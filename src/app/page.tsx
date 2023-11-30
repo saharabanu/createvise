edfa3226c361b73e8ec5b637484783a45c4bf44a
saharabanu <saharabanungn@gmail.com>
2023-11-30 11:44:39 +0600

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import Freedom from "@/component/ui/Freedom";
 import Navbar from "@/component/ui/Navbar";
 import { useEffect, useState } from "react";
 import variables from "./variables.module.scss";
+import { useSpring, animated } from 'react-spring';
+
 
 
 export default function Home() {
@@ -15,27 +17,44 @@ export default function Home() {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
+  const navbarProps = useSpring({
+    transform: `translateY(${offsetY * 0.2}px)`,
+  });
+
+  const bannerProps = useSpring({
+    transform: `translateY(${offsetY * 0.3}px)`,
+  });
+
+  const creativityProps = useSpring({
+    transform: `translateY(${offsetY * 0.1}px)`,
+  });
+
+  // const freedomProps = useSpring({
+  //   transform: `translateY(${offsetY * 0.4}px)`,
+  // });
+
+  const footerProps = useSpring({
+    transform: `translateY(0)`,
+  });
 
   return (
-    <div style={{ transform: `translateY(${offsetY * 0.5}px)` }}>
-      <>
-        <div>
-          <Navbar />
-         
-       
-        
-       
-          
-
-          <Banner />
-          
-          <Creativity />
- 
-          <Freedom />
-          
-          <Footer /> 
-        </div>
-      </>
+    <div>
+      <animated.div style={navbarProps}>
+        <Navbar />
+      </animated.div>
+      <animated.div style={bannerProps}>
+        <Banner />
+      </animated.div>
+      <animated.div style={creativityProps}>
+        <Creativity />
+      </animated.div>
+      <Freedom />
+      {/* <animated.div style={freedomProps}>
+        <Freedom />
+      </animated.div> */}
+      <animated.div style={footerProps} >
+        <Footer />
+      </animated.div>
     </div>
   );
 }
